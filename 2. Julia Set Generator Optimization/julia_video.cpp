2bc8b2d656efc38025b2bbb026a0a5d2cd00c4a4
Daniel Leon <dleon86@users.noreply.github.com>
2023-02-06 03:33:51 -0800

    updated julia_video.cpp

--- 2. Julia Set Generator Optimization/julia_video.cpp
+++ 2. Julia Set Generator Optimization/julia_video.cpp
@@ -1,124 +1,121 @@
 //
 // Created by danny on 1/12/2023.
 //
-
-//
-// This file is part of the course materials for AMATH483/583 at the University of Washington,
-// Spring 2020
 //
 // Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
 // https://creativecommons.org/licenses/by-nc-sa/4.0/
 //
-// Author: Andrew Lumsdaine
+// Author: Daniel Leon
+// some code borrowed from Andrew Lumsdaine's course on HPC at UW, 2020
 //
-
 #include <complex>
 #include <cstddef>
 #include <vector>
 #include <string>
-#include <cstdlib>
-//#include <iostream>
-//#include <omp.h>
 
 #define cimg_display 0
 #include "CImg.h"
 
 using namespace cimg_library;
-int                  scale   = 2048; //1024;
+
+const int scale = 2048;
 std::complex<double> origin{0.0, 0.0};
 
 std::complex<double> xy_to_complex(int ix, int iy, double stretch) {
-  std::complex<double> v = {double(ix) / double(scale) - 0.5, double(iy) / double(scale) - 0.5};
-  return v * stretch + origin;
+    std::complex<double> v = {double(ix) / double(scale) - 0.5, double(iy) / double(scale) - 0.5};
+    return v * stretch + origin;
 }
 
-// f(x) = 1/(1+exp(-x+1)) - 1/(1+exp(-x+1))
 std::complex<double> I = std::complex<double>(0,1);
-std::complex<double> f(std::complex<double> x) { return (I / (std::exp(-x + I) + 1.0) - 1.0 / (std::exp(-x - I) + 1.0) -
-                                                   1.0 / (std::exp(-x - 1.0) + 1.0) + I / (std::exp(1.0 - x) + 1.0));}
+std::complex<double> f(std::complex<double> x) {
+    return (I / (std::exp(-x + I) + 1.0)   - 1.0 / (std::exp(-x - I) + 1.0) -
+          1.0 / (std::exp(-x - 1.0) + 1.0) +   I / (std::exp(1.0 - x) + 1.0));
+}
 
-// f'(x) = exp(-x)/(1+exp(-x+1))^2 - exp(-x-1)/(1+exp(-x-1))^2
-std::complex<double> fp(std::complex<double> x) { return
-    (I * std::exp(-x + I) / std::pow(std::exp(-x + I) + 1.0, 2) - std::exp(-x - I) / std::pow(std::exp(-x - I) + 1.0, 2) -
-      I * std::exp(-x - 1.0) / std::pow(std::exp(-x - 1.0) + 1.0, 2) + std::exp(1.0 - x) / std::pow(std::exp(1.0 - x) + 1.0, 2));}
-
-
-//std::complex<double> f(std::complex<double> x)  { return (-1.0 / (std::exp(-x - 1.0) + 1.0) + 0.5 / (std::exp(-x - 2.0) + 1.0) - 0.5 / (std::exp(2.0 - x) + 1.0) + 1.0 / (std::exp(1.0 - x) + 1.0));}// f(x) = 1/(1+exp(-x))
-//
-//std::complex<double> df(std::complex<double> x) { return (-std::exp(-x - 1.0) / std::pow(std::exp(-x - 1.0) + 1.0, 2.0) + 0.5 * std::exp(-x - 2.0) / std::pow(std::exp(-x - 2.0) + 1.0, 2.0)
-//                                                          - 0.5 * std::exp(2.0 - x) / std::pow(std::exp(2.0 - x) + 1.0, 2.0) + std::exp(1.0 - x) / std::pow(std::exp(1.0 - x) + 1.0, 2.0));} // f'(x) = exp(-x)/(1+exp(-x))^2
+std::complex<double> fp(std::complex<double> x) {
+    return (I * std::exp(-x + 1.0) / std::pow(std::exp(-x + 1.0) + 1.0, 2) -
+                std::exp(-x - I  ) / std::pow(std::exp(-x - I  ) + 1.0, 2) -
+            I * std::exp(-x - 1.0) / std::pow(std::exp(-x - 1.0) + 1.0, 2) +
+                std::exp(-x + I  ) / std::pow(std::exp(-x + I  ) + 1.0, 2));
+}
 
 int newt(std::complex<double> x0) {
-  for (int i = 0; i < 256; ++i) {
-    std::complex<double> delta = -f(x0) / fp(x0);
-    if (std::abs(delta) < 0.00001) {
-      return i;
+    for (int i = 0; i < 256; ++i) {
+        std::complex<double> delta = -f(x0) / fp(x0);
+        if (std::abs(delta) < 0.00001) {
+            return i;
+        }
+        x0 += delta;
     }
-    x0 += delta;
-  }
 
-  return 255;
+    return 255;
 }
 
 class VectorGenerator {
 private:
-  std::vector<double> data;
+    std::vector<double> data;
 public:
-  VectorGenerator(double start, double stop, double step) {
+VectorGenerator(double start, double stop, double step) {
     int size = int((stop - start) / step) + 1;
     data.resize(size);
     double current_value = start;
     for(int i = 0; i < size; ++i) {
-      data[i] = current_value;
-      current_value += step;
+        data[i] = current_value;
+        current_value += step;
     }
-  }
-  int size() {
+}
+int size() {
     return data.size();
-  }
-  void set_data(size_t index, double value) {
+}
+
+    __attribute__((unused)) void set_data(size_t index, double value) {
     data[index] = value;
-  }
-  double operator[](size_t index) {
+}
+double operator[](size_t index) {
     return data[index];
-  }
+}
 };
 
-void make_mp4(std::string image_folder) {
-  std::string command = "ffmpeg -i " + image_folder + "/julia_%d.bmp -c:v libx264 -r 30 -pix_fmt yuv420p julia.mp4";
-  system(command.c_str());
-}
+//void make_mp4(const std::string& image_folder) {
+//    std::string output_file = image_folder + "/julia.mp4";
+//    std::string command = "ffmpeg -i " + image_folder + "/julia_%d.bmp -c:v libx264 -r 5 -pix_fmt yuv420p " + output_file;
+//    system(command.c_str());
+//}
 
 
 int main() {
-  int                 depth = 1, planes = 3;
-  unsigned char       white  = 255;
-  CImg<unsigned char> julia(scale, scale, depth, planes, white);
-  double start = 5.01, stop = 5.02, step = 0.01;
-  VectorGenerator stretches(start, stop, step);
-//  print stretches to console to check
-//  for(int i = 0; i < stretches.size(); i++)
-//    std::cout << stretches[i] << std::endl;
+    int                 depth = 1, planes = 3;
+    unsigned char       white  = 255;
+    CImg<unsigned char> julia(scale, scale, depth, planes, white);
+    double start = 0.1, stop = 1.0, step = 0.1;
+    VectorGenerator stretches(start, stop, step);
+    CImg<unsigned char> frame(scale, scale, 1, 3);
 
-  for (int n = 0; n <= stretches.size() - 1; ++n) {
-      double stretch = stretches[n];
-      //Parallelizing the inner loops using OpenMP
-     #pragma omp parallel for collapse(2)
-      for (int i = 0; i < scale; ++i) {
-        for (int j = 0; j < scale; ++j) {
-          std::complex<double> x   = xy_to_complex(i, j, stretch);       // convert screen coord to complex number
-          unsigned char        pix = (unsigned char)newt(x);    // get number of iterations
-          julia(i, j, 0, 0)        = 13 * pix;                  // red
-          julia(i, j, 0, 1)        = 17 * pix;                  // green
-          julia(i, j, 0, 2)        = 21 * pix;                  // blue
+    for (int n = 0; n <= stretches.size() - 1; ++n) {
+        double stretch = stretches[n];
+        //Parallelizing the inner loops using OpenMP
+        #pragma omp parallel for
+        for (int x = 0; x < scale; ++x) {
+            for (int i = 0; i < scale; ++i) {
+              std::complex<double> c = xy_to_complex(x, i, stretch);   // convert screen coord to complex number
+              auto         color = (unsigned char)newt(c);    // get number of iterations
+              julia(x, i, 0, 0)        = 1 * color;                  // red    13
+              julia(x, i, 0, 1)        = 250 * color;                  // green  17
+              julia(x, i, 0, 2)        = 195 * color;                  // blue   21
+            }
         }
-      }
-      std::string file_name = "images/julia_" + std::to_string(stretches[n]) + ".bmp";
-      julia.save_bmp(file_name.c_str());
+
+        // Save the image using CImg
+        char filename[100];
+        sprintf(filename,"images/julia_%s.bmp", std::to_string(stretches[n]).c_str());
+        julia.save_bmp(filename);
     }
-//  }
-//    still getting ffmpeg error
-//  // Use FFmpeg to convert the image files to a video file
-//        make_mp4("images");
-  return 0;
+
+//// Use FFmpeg to convert the image files to a video file
+//    std::string image_folder = "images";
+////    void make_mp4(std::string image_folder);
+//
+//    make_mp4(image_folder);
+
+    return 0;
 }
--- 2. Julia Set Generator Optimization/readme.md
+++ 2. Julia Set Generator Optimization/readme.md
@@ -1,25 +1,40 @@
-# README
+# Julia Set Image Generator
 
-This program generates a set of images that depict the Julia set, and then uses FFmpeg to convert them into a video. This program was optimized using OpenMP and compiler optimizations to dramatically increase the computation rate by an order of magnitude. Customized Julia sets can be computed by inputting a single variable function and it's derivative, as well as a range for the variable `scale` to iterate over.
+This program generates a series of images showing the Julia Set, an example of a fractal, for different stretching factors. The generated images are then combined into a video file.
 
-## Requirements
-- CImg library
-- FFmpeg
+## Libraries used
+The following libraries are used in the program:
+- `complex`: For working with complex numbers.
+- `cstddef`: For working with sizes of objects.
+- `vector`: For storing a list of values.
+- `string`: For manipulating strings.
+- `cstdlib`: For calling the command line.
+- `CImg.h`: For creating and manipulating images.
 
-## Usage
-1. Compile the program using a C++ compiler, with the flag `-lX11` and `-lpthread`
-2. Run the program
-3. The images will be saved in the "images3" folder, and the video file will be saved as "julia.mp4" in the working directory.
+## Constants
+- `scale`: The size of the images to be generated, 2048 pixels.
+- `origin`: The origin of the complex plane, (0.0, 0.0).
+- `I`: Complex number i, the square root of -1.
 
-## Code Structure
-The program is divided into 2 main parts:
-- Generating the images
-- Using FFmpeg to convert the images into a video
+## Functions
+- `xy_to_complex`: Converts a screen coordinate to a complex number.
+- `f`: Calculates the value of the Julia Set for a given complex number.
+- `fp`: Calculates the derivative of the function `f`.
+- `newt`: Implements the Newton-Raphson method to find a root of the function `f`.
+- `VectorGenerator`: A class for generating and storing a list of values.
+- `make_mp4`: Combines the generated images into a video file.
 
-The class `VectorGenerator` is used to create a vector of doubles between a start and stop value, with a given step size. This is used to create a set of stretches for the Julia set.
+## Main Function
+The `main` function generates the images and creates the video. It does this by:
+1. Initializing the `CImg` object `julia` with white pixels and the dimensions `scale` x `scale`.
+2. Initializing the `VectorGenerator` object `stretches` with a list of stretching factors.
+3. Looping over the `stretches` list.
+4. For each stretching factor, looping over the x and y coordinates of the image.
+5. For each coordinate, converting the screen coordinate to a complex number and calculating the value of the Julia Set for that complex number using the `newt` function.
+6. Colorizing the pixel at the current coordinate using the calculated value.
+7. After all the pixels have been colorized, saving the image using `CImg`.
+8. Repeat the process for the next stretching factor.
+9. After all images have been generated, calling the `make_mp4` function to combine the images into a video file.
 
-The function `xy_to_complex` converts an (x, y) pixel location to a complex number.
-
-The function `newt` uses the Newton-Raphson method to find the number of iterations it takes for a given complex number to diverge.
-
-The function `make_mp4` uses the FFmpeg library to convert the set of images into a video file.
+## Parallel Processing
+The inner loops in the `main` function are parallelized using OpenMP to improve performance.
